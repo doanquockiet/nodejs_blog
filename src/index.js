17003ed1dd0981abf415f2156cde32ab3c7e8f3d
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-06-30 09:58:36 +0700

    update resource

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const handlebars = require('express-handlebars')
+const express = require('express');
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/// HTTP logger
+app.use(morgan('combined'))
+/// template handlerbar
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.set('views',path.join(__dirname, 'resource/views'));
+
+
+app.get('/', (req, res) => {
+ res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+ })
+ 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
